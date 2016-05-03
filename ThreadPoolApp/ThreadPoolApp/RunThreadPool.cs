b4908176b6ff20066eb7f66c4ec540c91cbb3690
vassmate <vassmate08@gmail.com>
2016-05-03 14:23:04 +0200

    Added ThreadPoolApp

---
+++ ThreadPoolApp/ThreadPoolApp/RunThreadPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolApp
+{
+    static class RunThreadPool
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+
+            Console.Read();
+        }
+
+        static void ShowMyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine(">Thread: {0}, {1}", Thread.CurrentThread.ManagedThreadId, myText);
+        }
+    }
+}
